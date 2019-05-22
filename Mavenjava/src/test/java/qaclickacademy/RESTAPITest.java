5b1ed420f5f498ffcdc50e473becdb899ba60835
Hari <muvvaladevops@gmail.com>
2019-05-22 16:57:23 -0400

    Commit message

---
+++ Mavenjava/src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void psotjira()
+	{
+		
+	}
+	
+	@Test
+	public void deletetwitter()
+	{
+		
+	}
+}
